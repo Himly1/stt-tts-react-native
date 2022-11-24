b6796e791a6dddd2431b0685be23b3e197ae2763
zheng Ma <z1399956473@gmail.com>
2022-11-24 19:32:13 +0800

    init

---
+++ android/src/main/java/com/sttttsreactnative/SttTtsReactNativeModule.java
@@ -0,0 +1,32 @@
+package com.sttttsreactnative;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = SttTtsReactNativeModule.NAME)
+public class SttTtsReactNativeModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "SttTtsReactNative";
+
+  public SttTtsReactNativeModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  SttTtsReactNativeExample
+//
+
+import Foundation
